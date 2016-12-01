6b25ce68a20c2728aa421ab2675fcacbd7cca9b8
Kerey Roper <kereyr@eatsa.com>
2016-12-01 14:44:21 -0800

    don't need a hammer with two claws, nor npm

--- npm/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if ! which npm &> /dev/null; then
-    echo "Installing npm"
-    sudo curl -L https://npmjs.org/install.sh | sh
-fi
-echo "Installing npm packages"
-sudo npm install -g gulp
-sudo npm install -g grunt-cli
